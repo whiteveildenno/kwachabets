c553c06109a7af697d2af4d3367c2252d22d4a8b
dennis <dennisgitau4540@gmail.com>
2021-04-01 18:47:21 +0300

    admin settings adjust

--- tips/admin.py
+++ tips/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from  .models import Tip
 # Register your models here.
 class TipAdmin(admin.ModelAdmin):
-  list_display = ('kick_off_time', 'competition_name', 'home_team', 'away_team', 'prediction', 'match_odds','is_published')
+  list_display = ('kick_off_time', 'date_of_match', 'competition_name', 'home_team', 'away_team', 'prediction', 'match_odds','is_published')
   list_display_links = ('kick_off_time', 'competition_name')
   list_filter = ('competition_name','kick_off_time',)
   list_editable = ('is_published',)
   search_fields = ('competition_name', 'home_team', 'away_team')
-  list_per_page = 25
+  list_per_page = 12
 
 admin.site.register(Tip, TipAdmin)
