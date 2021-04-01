d6b3d6b1228f1106dbdcdb8ef623da046622e086
dennis <dennisgitau4540@gmail.com>
2021-04-01 16:16:20 +0300

    add.txt

--- officialkwachabets/urls.py
+++ officialkwachabets/urls.py
@@ -1,6 +1,7 @@
-
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,4 +10,6 @@ urlpatterns = [
     path('', include('bookmakers.urls')),
     path('', include('contacts.urls')),
     path('admin/', admin.site.urls),
+    path("ads.txt", RedirectView.as_view(url=staticfiles_storage.url("ads.txt")),
+    ),
 ] + static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
